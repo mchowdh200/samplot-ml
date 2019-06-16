a0da4001767ba005e876a20db9d724366fddf8d9
Murad <mchowdh200@gmail.com>
2019-06-16 12:36:19 -0600

    Modifying data loader for our new dataset.

--- utils.py
+++ utils.py
@@ -1,22 +1,27 @@
+import pathlib
 import numpy as np
 import pandas as pd
 import tensorflow as tf
 
 
-def get_filenames(df):
+def get_filenames(data_dir='./data/cropped'):
     """
-    Get filenames of images which are composed of fields in the dataframe
+    Get filenames of images from provided root directory.
     """
-    return f'./data/crop/{df.SVTYPE}_{df.chrom}_{df.start}-{df.end}.crop.png'
+    data_root = pathlib.Path(data_dir)
+    return [str(path) for path in list(data_root.glob('*.png'))]
 
-def get_labels(df, threshold=0.5):
+
+def get_labels(filenames):
     """
-    Apply labels to the dataframe. only consider DELs with a confidence 
-    greater than the threshold.
+    Filnames are in the following format:
+        <chrm>_<start>_<end>_<sample>_<genotype>.png
+    We will use the genotypes as the labels.
     """
-    if df.SVTYPE == 'DEL' and df.score > threshold:
-        return 1
-    return 0
+    labels = [f.split('_')[4].split('.')[0] for f in filenames]
+    label_to_index = {'ref': 0, 'het': 1, 'alt': 2}
+
+    return [label_to_index(l) for l in labels]
 
 
 def load_image(path, label):
@@ -30,24 +35,26 @@ def load_image(path, label):
     return tf.image.decode_png(image, channels=3)/255, label
 
 
-def get_dataset(data_dir='./data', batch_size=32):
+def get_dataset(data_dir='./data/cropped', batch_size=32):
     """
     Create a tensorflow dataset that yields image/label pairs to a model
     """
 
-    df = pd.read_csv(f"{data_dir}/mean_score.bed", sep='\t',
-                     names=['chrom', 'start', 'end', 'SVTYPE', 'score'])
+    # df = pd.read_csv(f"{data_dir}/mean_score.bed", sep='\t',
+    #                  names=['chrom', 'start', 'end', 'SVTYPE', 'score'])
 
-    n_images = len(df)
+    # n_images = len(df)
 
-    df['filename'] = df.apply(get_filenames, axis=1)
-    df['label'] = df.apply(get_labels, axis=1)
+    # df['filename'] = df.apply(get_filenames, axis=1)
+    # df['label'] = df.apply(get_labels, axis=1)
 
-    # construct the dataset
     AUTOTUNE = tf.data.experimental.AUTOTUNE
 
+    filenames = get_filenames(data_dir)
+    labels = get_labels(filenames)
+
     ds = tf.data.Dataset.from_tensor_slices(
-        (df.filename.values, tf.cast(df.label.values, tf.int64)))
+        (filenames, tf.cast(labels, tf.int64)))
 
     ds = ds.map(load_image)
     ds = ds.cache()
@@ -59,24 +66,25 @@ def get_dataset(data_dir='./data', batch_size=32):
     
 
 if __name__ == '__main__':
-    # test out the dataset code with a barebones keras model
-    BATCH_SIZE = 32
-    ds, n_images = get_dataset(batch_size=BATCH_SIZE)
-    # it = iter(ds)
-    # print(next(it)[0].shape)
+    # # test out the dataset code with a barebones keras model
+    # BATCH_SIZE = 32
+    # ds, n_images = get_dataset(batch_size=BATCH_SIZE)
+    # # it = iter(ds)
+    # # print(next(it)[0].shape)
 
-    model = tf.keras.Sequential([
-        tf.keras.layers.Flatten(),
-        tf.keras.layers.Dense(32, activation='relu'),
-        tf.keras.layers.Dense(2, activation='sigmoid'),
-    ])
+    # model = tf.keras.Sequential([
+    #     tf.keras.layers.Flatten(),
+    #     tf.keras.layers.Dense(32, activation='relu'),
+    #     tf.keras.layers.Dense(2, activation='sigmoid'),
+    # ])
 
-    model.compile(loss='categorical_crossentropy', optimizer='Adam', metrics=['Accuracy'])
-    model.fit(ds, 
-              steps_per_epoch=np.ceil(n_images/BATCH_SIZE),
-              epochs=10)
+    # model.compile(loss='categorical_crossentropy', optimizer='Adam', metrics=['Accuracy'])
+    # model.fit(ds, 
+    #           steps_per_epoch=np.ceil(n_images/BATCH_SIZE),
+    #           epochs=10)
 
-
+    filenames = get_filenames()
+    print(get_labels(filenames)[:5])
 
 
 
