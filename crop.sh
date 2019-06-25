10e8a2451ecc2e766b563582c08c1bad7fbded35
Murad Chowdhury <much8161@fiji-2.colorado.edu>
2019-06-25 15:13:36 -0600

    added python script used to get sv regions.

--- crop.sh
+++ crop.sh
@@ -4,8 +4,8 @@
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=much8161@colorado.edu
 #SBATCH --nodes=1
-#SBATCH --ntasks=64
-#SBATCH --mem=32gb
+#SBATCH --ntasks=1
+#SBATCH --mem=4gb
 #SBATCH --time=24:00:00               # Time limit hrs:min:sec
 #SBATCH --output=/Users/much8161/Repositories/samplot-ml/out/crop.out
 #SBATCH --error=/Users/much8161/Repositories/samplot-ml/out/crop.err
@@ -21,12 +21,16 @@ function crop
     b=`basename $p .png`
     o=crop/${b}.png
 
-    convert \
-        -crop 2090x575+175+200 \
-        -fill white \
-        -draw "rectangle 0,30 500,50" \
-        -draw "rectangle 600,0 700,50" \
-        $p $o
+    if [ ! -f $o ]; then
+        # crop image if we haven't yet
+        convert \
+            -crop 2090x575+175+200 \
+            -fill white \
+            -draw "rectangle 0,30 500,50" \
+            -draw "rectangle 600,0 700,50" \
+            $p $o
+    fi
+
     echo $o
 
 }
@@ -39,6 +43,6 @@ DATA_DIR=/scratch/Shares/layer/projects/samplot/ml/data/1kg/high_cov
 
 cd $DATA_DIR
 
-ls $DATA_DIR/imgs | gargs -p 64 'crop {}'
+ls $DATA_DIR/imgs | gargs --log ~/Repositories/samplot-ml/log/log.txt 'rj -l /dev/null -n {0} -c "crop {0}"'
 
 cd $SCRIPT_DIR
