994eb009440fda9fb19df1d8406f66ff592c3ee8
Murad <mchowdh200@gmail.com>
2019-08-27 11:28:12 -0600

    Added command line arg parsing to pr curve script.

--- visualization/plot_pr_curve.py
+++ visualization/plot_pr_curve.py
@@ -1,29 +1,33 @@
-import sys
-import numpy as np
+import argparse
 import pandas as pd
 import matplotlib.pyplot as plt
 
-title = sys.argv[1]
-legend = [
-    sys.argv[2],
-    sys.argv[3],
-]
+parser = argparse.ArgumentParser()
 
-for filename in sys.argv[4:]:
+parser.add_argument('-t', '--title', dest='title', type=str, default="")
+parser.add_argument('-l', '--legend', dest='legend', nargs='+')
+parser.add_argument('-f', '--filenames', dest='filenames', nargs='+')
+parser.add_argument('-s', '--saveto', dest='saveto', 
+                    required=False, default=None)
+args = parser.parse_args()
+
+for filename in args.filenames:
     df = pd.read_csv(
         filename, sep=' ', 
-        names=['threshold', 'precision', 'recall', 'f1']
-    )
-    # x = df[df.precision > 0].recall.values
-    # y = df[df.precision > 0].precision.values
+        names=['threshold', 'precision', 'recall', 'f1'])
     x = df.recall.values
     y = df.precision.values
     plt.plot(x, y , drawstyle='steps-post')
 
+plt.title(args.title)
 plt.xlabel('recall')
 plt.ylabel('precision')
-plt.legend(legend)
-plt.show()
+plt.legend(args.legend)
+
+if args.saveto:
+    plt.savefig(args.saveto)
+else:
+    plt.show()
 
 
 
