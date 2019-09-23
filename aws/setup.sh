6c5d53145de5f566d0b4b1424bcb13e702794ab9
Ubuntu <ubuntu@ip-172-31-28-8.ec2.internal>
2019-09-23 18:01:58 +0000

    source vimrc from neovim in setup script.

--- aws/setup.sh
+++ aws/setup.sh
@@ -31,6 +31,7 @@ sudo apt-get update -y
 sudo apt-get install -y neovim
 git clone https://github.com/mchowdh200/.vim.git ~/.vim
 mkdir ~/.config
+mkdir ~/.config/nvim
 printf "set runtimepath^=~/.vim runtimepath+=~/.vim/after\nlet &packpath=&runtimepath\nsource ~/.vimrc" > ~/.config/nvim/init.vim
 pip install jedi neovim
 echo "alias vim=nvim" >> ~/.profile
