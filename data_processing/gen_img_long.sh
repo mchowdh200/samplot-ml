a6c41ff6fd72b2b9293aee4999547a8a3279a544
Ubuntu <ubuntu@ip-172-31-28-8.ec2.internal>
2019-09-25 14:39:11 +0000

    "final" version of gen_img_long.sh

--- data_processing/gen_img_long.sh
+++ data_processing/gen_img_long.sh
@@ -86,15 +86,10 @@ function get_img {
     # feed the regions from sample & chr to samplot
     grep -P "(?=.*$chr\t)(?=.*$sample)" $regions_bed | gargs \
         "samplot_cmd {0} {1} {2} {3} {4} $bam_file $out_dir"
-    # rm $bam_file
-    # rm $bai_file
+    rm $bam_file
+    rm $bai_file
 }
 export -f get_img
-
-
-
-
-
 
 cd $bam_dir
 
@@ -102,29 +97,7 @@ samples=(HG00514 HG00733 NA19240)
 chromosomes=($(echo chr{{1..22},X}))
 
 for sample in ${samples[@]}; do
-    printf '%s\n' "${chromosomes[@]}" | head -4 | gargs -p 4 "get_img {} $sample $regions_bed $bam_list $bam_dir $out_dir"
-
-    exit
-    # TODO instead of for loop just printf '%s\n' "${chromosome[@]}" array into gargs
-    # TODO write a function containing the loop body
-    # for chr in $chromosomes; do
-    #     # get bams
-    #     bam_url=$(grep -P "(?=.*${chr}\.)(?=.*$sample)" $bam_list)
-    #     bam_file=$(basename $bam_url)
-    #     bai_url=$(dirname $bam_url)/$(basename $bam_url .bam).bai
-    #     bai_file=$(basename $bai_url)
-
-    #     # TODO make sure to export AWS keys before running this script
-    #     [[ ! -f $bam_file ]] && aws s3 cp $bam_url $bam_dir
-    #     [[ ! -f $bai_file ]] && aws s3 cp $bai_url $bam_dir
-
-
-    #     # feed the regions from sample & chr to samplot
-    #     # grep -P "(?=.*$chr\t)(?=.*$sample)" $regions_bed | gargs -p 1 "echo {}"
-    #     grep -P "(?=.*$chr\t)(?=.*$sample)" $regions_bed | gargs \
-    #         "samplot_cmd {0} {1} {2} {3} {4} $bam_file $out_dir"
-    #     exit
-    # done
+    printf '%s\n' "${chromosomes[@]}" | gargs -p 4 "get_img {} $sample $regions_bed $bam_list $bam_dir $out_dir"
 done
 
 cd -
