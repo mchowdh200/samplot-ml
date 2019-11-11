3bfb0c7187b9cb3984dc4d60b4ed9bead9d5aeb6
Murad <mchowdh200@gmail.com>
2019-11-11 15:58:54 -0700

    script for genotyping variants on ec2 instance.

---
+++ data_processing/genotype_variants.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+set -e
+
+while (( "$#" )); do
+    case "$1" in
+        --fasta)
+            fasta=$2
+            shift 2;;
+        --cram-url)
+            cram_url=$2
+            shift 2;;
+        --crai-url)
+            crai_url=$2
+            shift 2;;
+        --download-dir)
+            download_dir=$2
+            shift 2;;
+        --out-dir)
+            out_dir=$2
+            shift 2;;
+        --s3-dist)
+            s3_dist=$2
+            shift 2;;
+    esac
+done
+
+# download the alignments
+cram=$(basename $cram_url)
+crai=$(basename $crai_url)
+sample=$(cut -d'.' -f1 $cram)
+aws s3 cp $cram_url $download_dir # don't forget to set environment variables
+
+# genotype with smoove. annotate with duphold
+mkdir $out_dir/$sample
+smoove call -p 2 \
+    --outdir $out_dir/$sample \
+    --name $sample \
+    --fasta $fasta \
+    --genotype \
+    --duphold \
+    $download_dir/$cram
+
+# copy results to s3
+aws s3 cp $out_dir/$sample/$sample-smoove.genotyped.vcf.gz $s3_dest/
+aws s3 cp $out_dir/$sample/$sample-smoove.genotyped.vcf.gz.csi $s3_dest/
+
+# delete local data
+rm -r $out_dir/$sample/
+rm $download_dir/$cram
+rm $download_dir/$crai
+
--- data_processing/get_regions.sh
+++ data_processing/get_regions.sh
@@ -8,11 +8,3 @@ out_dir=$2
 bcftools view -i 'SVTYPE="DEL"' $vcf \
     | bcftools query -f '%CHROM\t%POS\t%INFO/END[,%SAMPLE\t%GT]\n' \
     | python3 sample_del.py > $out_dir/1kg_regions.bed
-
-rm *.out *.err
-
-    # TODO
-    # get all 0/1 and 1/1 genotypes --> del.bed
-    # get sample of 0/0 genotypes --> ref.bed
-    # combine both to get
-
