f5794cf5b8b9f44d8e30e1889abb97ce3c56639c
Murad <mchowdh200@gmail.com>
2019-12-04 13:29:27 -0700

    Update ec2 setup script...
    * to install pysam that supports s3 stored BAM/CRAM
    * use the latest version of samplot.py

--- aws/setup.sh
+++ aws/setup.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-# set up an i3.large/xlarge instance with samplot and nvme mounted ssd
+# set up an ec2 instance with samplot and nvme mounted storage
 
 set exuo pipefail
 
-# some common dependencies
+# some common dependencies ------------------------------------------------------------------------
 sudo apt-get update
 sudo apt-get install -y build-essential git libfuse-dev libcurl4-openssl-dev libxml2-dev mime-support automake libtool pkg-config libssl-dev ncurses-dev awscli python-pip libbz2-dev liblzma-dev unzip openjdk-8-jre-headless
 
-# mount storage
+# mount storage -----------------------------------------------------------------------------------
 export TMPDIR=/mnt/local
 sudo mkfs -t ext4 /dev/nvme0n1
 sudo mkdir /mnt/local
@@ -15,14 +15,14 @@ sudo mkdir /mnt/local
 sudo mount /dev/nvme0n1 /mnt/local
 sudo chown ubuntu /mnt/local
 
-# anaconda
+# conda setup -------------------------------------------------------------------------------------
 wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/miniconda.sh
 bash ~/miniconda.sh -b -p $HOME/miniconda
 eval "$($HOME/miniconda/bin/conda shell.bash hook)"
 conda init
 conda config --add channels bioconda
 
-# tmux/neovim setup
+# tmux/neovim setup -------------------------------------------------------------------------------
 echo "source-file ~/.tmux.d/.tmux.conf" > ~/.tmux.conf
 git clone https://github.com/mchowdh200/.tmux.d.git ~/.tmux.d
 
@@ -37,19 +37,26 @@ pip install jedi neovim
 echo "alias vim=nvim" >> ~/.profile
 echo "export EDITOR=nvim" >> ~/.profile
 
-# setup path
+# setup path --------------------------------------------------------------------------------------
 mkdir /mnt/local/bin
 echo "PATH=$PATH:/mnt/local/bin" >> ~/.profile
 
 # setup conda environments
+# samplot -----------------------------------------------------------------------------------------
 conda create -y --name samplot
 conda activate samplot
-conda install -y samplot
+conda install -y python=3.6 matplotlib numpy cython
+git clone https://github.com/pysam-developers/pysam.git
+cd pysam
+python setup.py install
+git clone https://github.com/ryanlayer/samplot.git
+conda deactivate
 
+# smoove ------------------------------------------------------------------------------------------
 conda create -y --name smoove
 conda activate smoove
 conda install -y smoove
 
-# other misc utilities/config
+# other misc utilities/config ---------------------------------------------------------------------
 wget https://github.com/brentp/gargs/releases/download/v0.3.9/gargs_linux -O /mnt/local/bin/gargs
 chmod +x /mnt/local/bin/gargs
