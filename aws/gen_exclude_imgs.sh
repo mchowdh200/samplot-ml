efb1f22b1863ecd6371916ede56f4ea1c03de54e
Ubuntu <ubuntu@ip-172-31-28-164.ec2.internal>
2019-12-05 05:20:41 +0000

    finalizing gen_exclude_imgs script.

--- aws/gen_exclude_imgs.sh
+++ aws/gen_exclude_imgs.sh
@@ -7,21 +7,23 @@ mkdir /mnt/local/data/imgs
 n_tasks=$1
 
 # get ref
+echo "Downloading reference genome..."
 aws s3 cp s3://1000genomes/technical/reference/GRCh38_reference_genome/GRCh38_full_analysis_set_plus_decoy_hla.fa /mnt/local/ref/
 aws s3 cp s3://1000genomes/technical/reference/GRCh38_reference_genome/GRCh38_full_analysis_set_plus_decoy_hla.fa.fai /mnt/local/ref/
 
 # get the 1kg CRAIs
+echo "Downloading cram indices..."
 cat ../data_listings/1kg_high_cov_crams_s3.txt \
     | gargs -p 48 "aws s3 cp {}.crai /mnt/local/data/CRAM/"
 
 cat "../data_listings/1kg_smoove_genotyped_exclude.bed" \
-    | gargs -p $n_tasks "bash gen_img.sh \\
-        --chrom {0} --start {1} --end {2} --sample {3} --genotype REF \\
-        --min-mqual 5 \\
-        --fasta /mnt/local/data/ref/GRCh38_reference_genome/GRCh38_full_analysis_set_plus_decoy_hla.fa \\
-        --bam-list ../data_listings/1kg_high_cov_crams_s3.txt \\
-        --bam-dir /mnt/local/data/CRAM \\
-        --out-dir /mnt/local/data/imgs"
+    | gargs -p $n_tasks "bash ../data_processing/gen_img.sh \\
+    --chrom {0} --start {1} --end {2} --sample {3} --genotype REF \\
+    --min-mqual 5 \\
+    --fasta /mnt/local/data/ref/GRCh38_full_analysis_set_plus_decoy_hla.fa \\
+    --bam-list ~/samplot-ml/data_listings/1kg_high_cov_crams_s3.txt \\
+    --bam-dir /mnt/local/data/CRAM \\
+    --out-dir /mnt/local/data/imgs"
 
 aws s3 cp --recursive /mnt/local/data/imgs s3://layerlab/samplot-ml/1kg/exclude_ref_regions/
 
