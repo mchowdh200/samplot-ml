339969cfb041385231967b58d6172b957b09cc40
Murad <mchowdh200@gmail.com>
2019-12-12 10:57:36 -0700

    fixed generate_tensorflow_datasets script

--- data_processing/generate_tensorflow_datasets.sh
+++ data_processing/generate_tensorflow_datasets.sh
@@ -3,12 +3,13 @@
 set -eu
 
 training_set_name=$1
-ntasks=$2
+n_tasks=$2
 
 s3_source="s3://layerlab/samplot-ml/1kg/duphold_callset/crop/"
 s3_destination="s3://layerlab/samplot-ml/1kg/training_sets/${training_set_name}.$(date +%m.%d.%H)"
 
 data_dir=/mnt/local/data
+mkdir $data_dir
 mkdir $data_dir/crop
 mkdir $data_dir/train
 mkdir $data_dir/val
@@ -23,6 +24,9 @@ data_list=$(ls U $data_dir/crop)
 val_chrms=$(echo ^chr{1..3}_ | tr " " "|")
 echo "$data_list" | grep -v -E "$val_chrms" | shuf > $data_dir/train.txt
 echo "$data_list" | grep -E "$val_chrms" | shuf > $data_dir/val.txt
+
+aws s3 cp $data_dir/train.txt $s3_destination
+aws s3 cp $data_dir/val.txt $s3_destination
 
 # create the tfrecords
 python3 datasets.py $data_dir
