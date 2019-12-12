27d06a8fbd053f08082419f04b8333a2c6a417eb
Ubuntu <ubuntu@ip-172-31-29-210.ec2.internal>
2019-12-12 23:24:54 +0000

    fixed a typo.

--- aws/setup_tensorflow_gpu.sh
+++ aws/setup_tensorflow_gpu.sh
@@ -7,6 +7,12 @@ set exuo pipefail
 sudo apt-get update
 sudo apt-get install -y build-essential git libfuse-dev libcurl4-openssl-dev libxml2-dev mime-support automake libtool pkg-config libssl-dev ncurses-dev awscli  libbz2-dev liblzma-dev unzip
 
+# mount storage -----------------------------------------------------------------------------------
+sudo mkfs -t ext4 /dev/xvdf
+sudo mkdir /mnt/local
+sudo mount /dev/xvdf /mnt/local
+sudo chown ubuntu /mnt/local
+
 # nvidia stuff ------------------------------------------------------------------------------------
 # Add NVIDIA package repositories
 wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/cuda-repo-ubuntu1804_10.0.130-1_amd64.deb
@@ -18,30 +24,24 @@ sudo apt install ./nvidia-machine-learning-repo-ubuntu1804_1.0.0-1_amd64.deb
 sudo apt-get update
 
 # Install NVIDIA driver
-sudo apt-get install --no-install-recommends nvidia-driver-418
+sudo apt-get install -y --no-install-recommends nvidia-driver-418
 # Reboot. Check that GPUs are visible using the command: nvidia-smi
 
 # Install development and runtime libraries (~4GB)
-sudo apt-get install --no-install-recommends \
-    cuda-10-0 \
-    libcudnn7=7.6.2.24-1+cuda10.0  \
-    libcudnn7-dev=7.6.2.24-1+cuda10.0
+# sudo apt-get install -y --no-install-recommends \
+#     cuda-10-0 \
+#     libcudnn7=7.6.2.24-1+cuda10.0  \
+#     libcudnn7-dev=7.6.2.24-1+cuda10.0
 
 # Install TensorRT. Requires that libcudnn7 is installed above.
 # sudo apt-get install -y --no-install-recommends libnvinfer5=5.1.5-1+cuda10.0 \
 #     libnvinfer-dev=5.1.5-1+cuda10.0
 
-# mount storage -----------------------------------------------------------------------------------
-# sudo mkfs -t ext4 /dev/nvme0n1
-# sudo mkfs -t ext4 /dev/nvme1n1
-# sudo mkdir /mnt/local
-# sudo mount /dev/nvme1n1 /mnt/local
-# sudo chown ubuntu /mnt/local
 
 # conda setup -------------------------------------------------------------------------------------
 wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/miniconda.sh
-bash ~/miniconda.sh -b -p $HOME/miniconda
-eval "$($HOME/miniconda/bin/conda shell.bash hook)"
+bash ~/miniconda.sh -b -p /mnt/local/miniconda
+eval "$(/mnt/local/miniconda/bin/conda shell.bash hook)"
 conda init
 conda config --add channels bioconda
 
@@ -69,8 +69,8 @@ chmod +x /mnt/local/bin/gargs
 # samplot -----------------------------------------------------------------------------------------
 conda create -y --name tensorflow
 conda activate tensorflow
-conda install -y python=3.7 matplotlib numpy
-pip install tensorflow tensorflow-addons
+conda install -y python=3.7 pip matplotlib numpy
+pip install tensorflow-gpu tensorflow-addons
 conda deactivate
 
 # setup environment variables that will enable use of s3 buckets with tensorflow datasets ---------
