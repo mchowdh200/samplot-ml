a624adc3eea2dd74d67debdedaf923c73a7d4bb0
Murad Chowdhury <much8161@franklin.colorado.edu>
2020-02-10 10:55:04 -0700

    updated 1kg genotyping script to use more sensitve calls.

--- data_processing/genotype_variants.sh
+++ data_processing/genotype_variants.sh
@@ -10,18 +10,15 @@ while (( "$#" )); do
         --cram-url)
             cram_url=$2
             shift 2;;
-        --crai-url)
-            crai_url=$2
-            shift 2;;
-        --download-dir)
-            download_dir=$2
+        --header-contigs) # text file containing vcf header contigs
+            header_contigs=$2
             shift 2;;
         --out-dir)
             out_dir=$2
             shift 2;;
-        --s3-dist)
-            s3_dist=$2
-            shift 2;;
+        # --s3-dest)
+        #     s3_dist=$2
+        #     shift 2;;
         --) # end argument parsing
             shift
             break;;
@@ -31,28 +28,44 @@ while (( "$#" )); do
     esac
 done
 
+s3_dest="s3://layerlab/samplot-ml/1kg/lumpy_sensitive/"
+
 # download the alignments
 cram=$(basename $cram_url)
-crai=$(basename $crai_url)
-sample=$(cut -d'.' -f1 $cram)
-aws s3 cp $cram_url $download_dir # don't forget to set environment variables
+crai=$cram.crai
+sample=$(cut -d'.' -f1 <<<$cram)
 
-# genotype with smoove. annotate with duphold
-mkdir $out_dir/$sample
-smoove call -p 2 \
+[[ ! -d $out_dir/$sample ]] && mkdir $out_dir/$sample
+
+# don't forget to set AWS environment variables
+[[ ! -f $out_dir/$sample/$cram ]] && aws s3 cp $cram_url $out_dir/$sample 
+[[ ! -f $out_dir/$sample/$crai ]] && aws s3 cp $cram_url.crai $out_dir/$sample 
+
+# call sv's with smoove (really just want the split/disc. reads)
+smoove call \
     --outdir $out_dir/$sample \
     --name $sample \
     --fasta $fasta \
-    --genotype \
-    --duphold \
-    $download_dir/$cram
+    $out_dir/$sample/$cram
 
-# copy results to s3
-aws s3 cp $out_dir/$sample/$sample-smoove.genotyped.vcf.gz $s3_dest
-aws s3 cp $out_dir/$sample/$sample-smoove.genotyped.vcf.gz.csi $s3_dest
+# modify the original lumpy command output by smoove to be more sensitive
+tail -1 $out_dir/$sample/$sample-lumpy-cmd.sh \
+    | awk '{$6=2; $8=2; print $0}' > $out_dir/$sample/lumpy_sensitive.sh
 
-# delete local data
-rm -r $out_dir/$sample/
-rm $download_dir/$cram
-rm $download_dir/$crai
+# execute the lumpy command and genotype with svtyper
+bash $out_dir/$sample/lumpy_sensitive.sh \
+    | bcftools annotate -h $header_contigs \
+    | svtyper -B $out_dir/$sample/$cram \
+              -T $fasta \
+    | bcftools sort \
+    | bgzip -c > $out_dir/$sample/$sample.genotyped.vcf.gz
+tabix $out_dir/$sample/$sample.genotyped.vcf.gz
+    
+# copy vcf results to s3
+aws s3 cp $out_dir/$sample/$sample.genotyped.vcf.gz $s3_dest
+aws s3 cp $out_dir/$sample/$sample.genotyped.vcf.gz.tbi $s3_dest
+
+# cleanup bam/crams
+rm $out_dir/$sample/*.bam*
+rm $out_dir/$sample/*.cram*
 
