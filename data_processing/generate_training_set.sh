f534d4fbe1e044534a6ce37fed9257ca8a79b96b
Ubuntu <ubuntu@ip-172-31-65-110.ec2.internal>
2020-02-25 21:44:19 +0000

    fixes to training set generation script.

--- data_processing/generate_training_set.sh
+++ data_processing/generate_training_set.sh
@@ -1,23 +1,7 @@
-#!/bin/bash
-#SBATCH -p short
-#SBATCH --job-name=generate_training_set
-#SBATCH --mail-type=ALL
-#SBATCH --mail-user=much8161@colorado.edu
-#SBATCH --nodes=1
-#SBATCH --ntasks=32
-#SBATCH --mem=128gb
-#SBATCH --time=24:00:00
-#SBATCH --output=/Users/much8161/Repositories/samplot-ml/data_processing/gen_training.out
-#SBATCH --error=/Users/much8161/Repositories/samplot-ml/data_processing/gen_training.err
-
-# training_regions=/scratch/Shares/layer/projects/samplot/ml/data/1kg/high_cov/BED/training_regions.bed
-# fasta=/scratch/Shares/layer/ref/GRCh38_full_analysis_set_plus_decoy_hla/GRCh38_full_analysis_set_plus_decoy_hla.fa
-# cram_list=~/Repositories/samplot-ml/data_listings/1kg_ftp_cram_list.txt
-# cram_dir=/scratch/Shares/layer/projects/samplot/ml/data/1kg/high_cov/alignments/
-# out_dir=/scratch/Shares/layer/projects/samplot/ml/data/1kg/high_cov/imgs-zoom/
+set -u
 
 # s3_destination="s3://layerlab/samplot-ml/1kg/duphold_callset"
-s3_destination="s3://layerlab/samplot-ml/1kg/training_sets/1kg_exclude_sensitive/"
+s3_destination="s3://layerlab/samplot-ml/1kg/training_sets/1kg_exclude_sensitive"
 n_tasks=$1
 
 data_dir=/mnt/local/data
@@ -46,25 +30,17 @@ cram_list=/home/ubuntu/samplot-ml/data_listings/1kg_high_cov_crams_s3.txt
 cat $cram_list | gargs -p $n_tasks "aws s3 cp {}.crai $data_dir/cram"
 
 # generate the images
-cat $training_regions | gargs -p $n_tasks \
+cat $training_regions | head | gargs -p $n_tasks \
     "bash gen_img.sh \\
-        --chrom chr{0} --start {1} --end {2} --sample {3} --genotype {4} \\
+        --chrom {0} --start {1} --end {2} --sample {3} --genotype {6} \\
         --min-mqual 10 \\
         --fasta $fasta \\
         --bam-list $cram_list \\
         --bam-dir $data_dir/cram \\
         --out-dir $data_dir/imgs"
-# cat $training_regions | gargs -p $n_tasks \
-#     "bash gen_img.sh \\
-#         --chrom chr{0} --start {1} --end {2} --sample {3} --genotype {4} \\
-#         --min-mqual 5 \\
-#         --fasta $fasta \\
-#         --bam-list $cram_list \\
-#         --bam-dir $data_dir/cram \\
-#         --out-dir $data_dir/imgs"
 
 # crop the images
-ls $data_dir/imgs | crop.sh -p $n_tasks -d $data_dir/crop
+ls $data_dir/imgs | bash crop.sh -p $n_tasks -d $data_dir/imgs -o $data_dir/crop
 
 # upload results to s3
 aws s3 cp --recursive $data_dir/imgs $s3_destination/imgs
