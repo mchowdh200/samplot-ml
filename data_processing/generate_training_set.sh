250e9596161b9019c26671f8390117ab4a02f053
Murad Chowdhury <much8161@franklin.colorado.edu>
2020-02-25 13:14:09 -0700

    update training set generation script.

--- data_processing/generate_training_set.sh
+++ data_processing/generate_training_set.sh
@@ -16,7 +16,8 @@
 # cram_dir=/scratch/Shares/layer/projects/samplot/ml/data/1kg/high_cov/alignments/
 # out_dir=/scratch/Shares/layer/projects/samplot/ml/data/1kg/high_cov/imgs-zoom/
 
-s3_destination="s3://layerlab/samplot-ml/1kg/duphold_callset"
+# s3_destination="s3://layerlab/samplot-ml/1kg/duphold_callset"
+s3_destination="s3://layerlab/samplot-ml/1kg/training_sets/1kg_exclude_sensitive/"
 n_tasks=$1
 
 data_dir=/mnt/local/data
@@ -29,8 +30,11 @@ mkdir $data_dir/imgs
 mkdir $data_dir/crop
 
 # get training regions
-aws s3 cp s3://layerlab/samplot-ml/1kg/duphold_callset/bed/training_regions_duphold.bed $data_dir/bed
-training_regions=$data_dir/bed/training_regions_duphold.bed
+aws s3 cp $s3_destination/ref.bed $data_dir/bed
+aws s3 cp $s3_destination/het.bed $data_dir/bed
+aws s3 cp $s3_destination/alt.bed $data_dir/bed
+cat $data_dir/bed/ref.bed $data_dir/bed/het.bed $data_dir/bed/alt.bed > $data_dir/bed/training_regions.bed
+training_regions=$data_dir/bed/training_regions.bed
 
 # get reference genome
 aws s3 cp s3://1000genomes/technical/reference/GRCh38_reference_genome/GRCh38_full_analysis_set_plus_decoy_hla.fa $data_dir/ref
@@ -41,12 +45,11 @@ fasta=$data_dir/ref/GRCh38_full_analysis_set_plus_decoy_hla.fa
 cram_list=/home/ubuntu/samplot-ml/data_listings/1kg_high_cov_crams_s3.txt
 cat $cram_list | gargs -p $n_tasks "aws s3 cp {}.crai $data_dir/cram"
 
-
 # generate the images
 cat $training_regions | gargs -p $n_tasks \
     "bash gen_img.sh \\
         --chrom chr{0} --start {1} --end {2} --sample {3} --genotype {4} \\
-        --min-mqual 5 \\
+        --min-mqual 10 \\
         --fasta $fasta \\
         --bam-list $cram_list \\
         --bam-dir $data_dir/cram \\
