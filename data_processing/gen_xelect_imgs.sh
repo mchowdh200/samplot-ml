cdd26bff3bbfc32507cf23474bf67ddaf3706924
Ubuntu <ubuntu@ip-172-31-78-235.ec2.internal>
2020-07-20 13:30:20 +0000

    finishing touches to gen_xelect_imgs.

--- data_processing/gen_xelect_imgs.sh
+++ data_processing/gen_xelect_imgs.sh
@@ -2,6 +2,7 @@
 
 data_dir="../data_listings"
 out_dir="/mnt/local/data"
+mkdir $out_dir $out_dir/imgs $out_dir/crop
 samples=( XA01 XA61 XA79 XE09 XE32 XH82 XH96 )
 
 # get the regions
@@ -15,10 +16,10 @@ done
 # generate images from regions
 sed -E -e 's/0\/1|1\/0/het/g' -e 's/1\/1/alt/g' -e 's/0\/0/ref/g' \
     $data_dir/SVplaudit_Xelect_full.del.bed |
-    gargs --dry-run -p 4 "bash gen_img.sh \\
+    gargs -p 4 "bash gen_img.sh \\
         --chrom {0} --start {1} --end {2} --sample {3} --genotype {4}-{5} \\
         --min-mqual 10 --bam-file s3://layerlabcu/xelect_bams/{3}.bam \\
-        -o /mnt/local/data"
+        -o /mnt/local/data/imgs"
 
 # for sample in ${samples[@]};do
 #     # get the regions
@@ -35,5 +36,8 @@ sed -E -e 's/0\/1|1\/0/het/g' -e 's/1\/1/alt/g' -e 's/0\/0/ref/g' \
 #     bash crop.sh -p 4 -d $out_dir/imgs -o $out_dir/crop
 # done
 
+
+bash crop.sh -p 4 -d $out_dir/imgs -o $out_dir/crop
+
 aws s3 cp --recursive $out_dir/imgs/ s3://layerlabcu/samplot-ml/salmon/xelect_full/imgs/
-aws s3 cp --recursive $out_dir/crop/ s3://layerlabcu/samplot-ml/salmon/xelect_ful/crop/
+aws s3 cp --recursive $out_dir/crop/ s3://layerlabcu/samplot-ml/salmon/xelect_full/crop/
