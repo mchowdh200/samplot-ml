006a01265fc1975bb8b115de9903e4cef1f69aa8
Ubuntu <ubuntu@ip-172-31-23-132.ec2.internal>
2020-09-15 21:52:08 +0000

    changes to generate_test_set_ec2.sh

--- data_processing/generate_test_set_ec2.sh
+++ data_processing/generate_test_set_ec2.sh
@@ -1,19 +1,18 @@
 #!/bin/bash
 
-set -e
+# set -e
 
 data_dir=$1
-s3_dest=$2
 samples=(HG00514 HG00733 NA19240)
 
 # get vcfs
 [[ ! -d $data_dir/VCF ]] && mkdir $data_dir/VCF
 aws s3 cp --recursive s3://layerlabcu/samplot-ml/chaisson/VCF/smoove/ $data_dir/VCF
-vcfs=$(find $data_dir/VCF -name '*.vcf')
+vcfs=$(find $data_dir/VCF -name '*.vcf.gz')
 
 # get reference
 [[ ! -d $data_dir/ref ]] && mkdir $data_dir/ref
-aws s3 cp --recursive s3://layerlabcu/samplot-ml/ref/ $data_dir/ref/
+aws s3 cp --recursive s3://layerlabcu/samplot-ml/ref/GRCh38_full_analysis_set_plus_decoy_hla/ $data_dir/ref/
 fasta=$data_dir/ref/GRCh38_full_analysis_set_plus_decoy_hla.fa
 
 # get alignments
@@ -23,6 +22,7 @@ bams=$(find $data_dir/BAM -name '*.cram')
 
 # generate images
 for sample in ${samples[@]}; do
+    echo $sample -------------------------------
     if [[ ! -d $data_dir/$sample ]]; then
         mkdir $data_dir/$sample
         mkdir $data_dir/$sample/imgs
