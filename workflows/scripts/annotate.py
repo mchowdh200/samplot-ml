93e8386a2b065f1ffef67ae9e22bd3651b3e6ecc
Murad <mchowdh200@gmail.com>
2021-03-30 12:29:22 -0600

    add actual annotation to annotate.py

--- workflows/scripts/annotate.py
+++ workflows/scripts/annotate.py
@@ -23,13 +23,38 @@ for l in open(bed, 'r'):
 # iterate over each record and replace the genotype
 # with the predicted genotypes
 with pysam.VariantFile('-', 'rb') as VCF:
+    # get the old genotype and prediction scores into the vcf FORMAT fields
+    VCF.header.add_meta('FORMAT', items=[('ID', 'OLDGT'),
+                                         ('Number', '1'),
+                                         ('Type', 'String'),
+                                         ('Description', 'Genotype before samplot-ml')])
+    VCF.header.add_meta('FORMAT', items=[('ID', 'PREF'),
+                                         ('Number', '1'),
+                                         ('Type', 'Float'),
+                                         ('Description', 'Samplot-ml P(0/0) prediction score')])
+    VCF.header.add_meta('FORMAT', items=[('ID', 'PHET'),
+                                         ('Number', '1'),
+                                         ('Type', 'Float'),
+                                         ('Description', 'Samplot-ml P(0/1) prediction score')])
+    VCF.header.add_meta('FORMAT', items=[('ID', 'PALT'),
+                                         ('Number', '1'),
+                                         ('Type', 'Float'),
+                                         ('Description', 'Samplot-ml P(1/1) prediction score')])
     print(str(VCF.header).strip())
     for variant in VCF:
-        key = '\t'.join([str(x) for x in [variant.contig,
+        region = '\t'.join([str(x) for x in [variant.contig,
                                           variant.pos,
                                           variant.stop]]) 
-        if key in predictions:
+        if region in predictions:
+            # annotate old gt, pref, phet, palt
+            oldgt = variant.samples[sample].allele_indices
+            pref, phet, palt = predictions[region]
+            variant.samples[sample]["OLDGT"] = f"{oldgt[0]}/{oldgt[1]}"
+            variant.samples[sample]["PREF"] = pref
+            variant.samples[sample]["PHET"] = phet
+            variant.samples[sample]["PALT"] = palt
+
+            # set new GT
             variant.samples[sample].allele_indices = genotypes[
-                np.argmax(predictions[key])]
-            if np.argmax(predictions[key]) > 0:
-                print(str(variant).rstrip())
+                np.argmax(predictions[region])]
+            print(str(variant).rstrip())
