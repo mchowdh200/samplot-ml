6bd97c4e9d2be0b3fabf89f37fb45625b11ba29c
Murad <mchowdh200@gmail.com>
2021-05-26 00:14:19 -0600

    clean up old files, and move to another repository for posterity.

--- evaluation/pred2bed.py
+++
@@ -1,8 +0,0 @@
-import sys
-
-with open(sys.argv[1], 'w') as bed_file:
-    for pred in sys.stdin:
-        chr, start, end = pred.split('_')[:3]
-        P = pred.split()[1:]
-        # print('\t'.join([chr, start, end] + P))
-        bed_file.write('\t'.join([chr, start, end] + P) + '\n')
